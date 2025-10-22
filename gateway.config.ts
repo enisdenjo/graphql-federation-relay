fafdf8240fd49c663537dfaa0ff57bc7d4bb2fbe
Denis Badurina <denis@denelop.com>
2025-10-22 13:11:04 +0200

    use node schema directly

--- gateway.config.ts
+++ gateway.config.ts
@@ -1,5 +1,22 @@
-import { defineConfig } from "@graphql-hive/gateway";
+import { defineConfig, type GatewayPlugin } from "@graphql-hive/gateway";
+import { executorFromSchema } from "@graphql-tools/executor";
+
+// @ts-expect-error no typedefs
+import { schema } from "./services/node/schema.mjs";
 
 export const gatewayConfig = defineConfig({
   supergraph: "./supergraph.graphql",
+  plugins: () => {
+    let nodeExecSet = false;
+    return [
+      {
+        onSubgraphExecute({ setExecutor, subgraphName }) {
+          if (subgraphName !== "node-resolver-service") return;
+          if (nodeExecSet) return; // set executor will be reused, needs to run only once
+          nodeExecSet = true;
+          setExecutor(executorFromSchema(schema));
+        },
+      } as GatewayPlugin,
+    ];
+  },
 });
